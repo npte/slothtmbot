d791bbae24c15261ea0bf7dab7fb167e13289b7e
kamazoh <kamazoh@inbox.ru>
2022-04-01 00:53:57 +0300

    Initial commit

---
+++ src/main/java/org/slothmud/tmbot/TmbotApplication.java
@@ -0,0 +1,13 @@
+package org.slothmud.tmbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/slothmud/tmbot/TmbotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.slothmud.tmbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
